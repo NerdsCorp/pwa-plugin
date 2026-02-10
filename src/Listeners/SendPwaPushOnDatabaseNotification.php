abca2e4087426ced3fe532cf28509fb1d502c2eb
NerdsCorpx <86178816+NerdsCorp@users.noreply.github.com>
2026-02-09 23:31:36 -0600

    i guess im getting tired

--- src/Listeners/SendPwaPushOnDatabaseNotification.php
+++ src/Listeners/SendPwaPushOnDatabaseNotification.php
@@ -13,6 +13,7 @@ use PwaPlugin\Services\PwaSettingsRepository;
 
 class SendPwaPushOnDatabaseNotification
 {
+
     public function __construct(
         private PwaSettingsRepository $settings,
         private PwaPushService $push,
